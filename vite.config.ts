39f9d69125982b726b4bfdc89d2fe832fae2b810
cancms <cancms@163.com>
2026-04-26 20:08:35 +0800

    google sheet data

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  // base: '/apexpbc/',  // 👈 关键配置：子路径
+  base: '/apexpbc/',  // 👈 关键配置：子路径
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
